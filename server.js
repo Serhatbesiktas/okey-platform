f286d9a51e0d361de8ae6df175d92796d2198c33
Serhatbesiktas <srtbsk34@gmail.com>
2026-06-10 08:56:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
 
-// Oyunun arayüz dosyalarını 'public' klasöründen çalıştırır
+// CORS ayarını ekliyoruz ki farklı tarayıcılar (Safari, Chrome) birbirini engellemesin
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
   console.log('Bir oyuncu bağlandı: ' + socket.id);
 
-  // Lobiden gelen mesajları dinle ve herkese anında yay
+  // Lobi sohbeti
   socket.on('lobi_mesaji_gonder', (data) => {
-    io.emit('lobi_mesaji_geldi', {
-      isim: data.isim,
-      mesaj: data.mesaj
-    });
+    io.emit('lobi_mesaji_geldi', { isim: data.isim, mesaj: data.mesaj });
+  });
+
+  // Masaya oturma eylemi
+  socket.on('masaya_otur', (data) => {
+    io.emit('sistem_mesaji', `${data.isim}, ${data.masaAdi} masasına oturdu!`);
   });
 
   socket.on('disconnect', () => {
@@ -22,7 +30,6 @@ io.on('connection', (socket) => {
   });
 });
 
-// Render'ın vereceği dinamik porta veya yerel 3000 portuna bağlanır
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log(`Sunucu ${PORT} portunda aktif...`);
